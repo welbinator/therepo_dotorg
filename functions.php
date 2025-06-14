9fbafa5924cf01eb40c14eb0a1f2c628a552ebae
James <james.welbes@gmail.com>
2025-06-13 23:50:36 -0500

    add github stars

--- functions.php
+++ functions.php
@@ -254,4 +254,33 @@ function fetch_latest_release_download_count($atts) {
 
 add_shortcode('number_of_downloads', __NAMESPACE__ . '\\fetch_latest_release_download_count');
 
+function github_star_count_shortcode($atts) {
+    $post_id = get_the_ID();
+
+    $github_owner = get_post_meta($post_id, 'github_username', true);
+    $github_repo  = get_post_meta($post_id, 'github_repo', true);
+
+    if (empty($github_owner) || empty($github_repo)) {
+        return '<p>Missing GitHub owner or repo name.</p>';
+    }
+
+    $api_url = "https://api.github.com/repos/{$github_owner}/{$github_repo}";
+    $cache_key = 'github_repo_meta_' . md5($api_url);
+    $data = \TheRepo\Functions\fetch_github_data($api_url, $cache_key);
+
+    if (isset($data['stargazers_count'])) {
+        $stars = number_format($data['stargazers_count']);
+        $star_link = "https://github.com/{$github_owner}/{$github_repo}/stargazers";
+
+        return '<div class="github-star-widget">
+                    <p><strong>' . esc_html($stars) . '</strong> ⭐</p>
+                    <a href="' . esc_url($star_link) . '" target="_blank" rel="noopener" class="github-star-button">Star on GitHub</a>
+                </div>';
+    }
+
+    return '<p>Unable to fetch star count.</p>';
+}
+add_shortcode('github_star_count', __NAMESPACE__ . '\\github_star_count_shortcode');
+
+
 
